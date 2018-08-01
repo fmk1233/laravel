6b6a99e090bb28cd160a155b30d80e8e03271792
matthew <894966033@qq.com>
2018-08-01 10:21:35 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@
 // Route::name('front.index')->get('/',function(){
 // 	return view("index");
 // });
+
+//定义单个路由绑定控制器的方法
 Route::get('/','IndexController@index');
 
-Route::get('/user', function(){
+//路由配中间件
+Route::get('/token', function(){
 	return view('welcome');
 })->middleware("token");
+
+//定义资源路由
+Route::resourece('user', 'UserController');
