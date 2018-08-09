564b93e49a1bc87b0f0d69426f942715ec6f5db3
matthew <894966033@qq.com>
2018-08-09 09:41:01 +0800

    users

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('user/{id}',function($id){
+Route::get('users/{id}',function($id){
 	return 'User'.$id;
 });
